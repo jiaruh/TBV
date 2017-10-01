0f668f79bb317478842a9946f482196a12cafec9
黄家儒 <jiaruh@cernet.edu.cn>
2017-10-01 23:53:53 +0800

    Initial Commit

---
+++ TBV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TBV
+//
+//  Created by jiaruh on 2017/10/1.
+//  Copyright © 2017年 jiaruh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TBV/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TBV
+//
+//  Created by jiaruh on 2017/10/1.
+//  Copyright © 2017年 jiaruh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
